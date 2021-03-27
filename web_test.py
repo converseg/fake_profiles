2706524645fcf72c9b25c394f51906b3760c00b7
Geoff Converse <converseg@gmail.com>
2021-03-27 13:09:25 -0500

    can load and save browser history in selenium

--- web_test.py
+++ web_test.py
@@ -4,55 +4,62 @@ import time
 import random
 import numpy as np
 import json
+import os
 
-capabilities = {
-  'browserName': 'firefox',
-  'browserVersion': '68.0',
-  'platformName': 'Windows 10',
-  'sauce:options': {}
-}
+#capabilities = {
+#  'browserName': 'firefox',
+#  'browserVersion': '68.0',
+#  'platformName': 'Windows 10',
+#  'sauce:options': {}
+#}
 
-# path = 'C://Users//gconverse//Desktop//comp_security'# //geckodriver'
+#use FF 87
+bin_path = 'C:/Program Files/Mozilla Firefox/firefox.exe'
+ff_binary = webdriver.firefox.firefox_binary.FirefoxBinary(firefox_path=bin_path)
+#use test_profile
+profile_path = './ff_profiles//test_profile'
+ff_profile = webdriver.firefox.firefox_profile.FirefoxProfile(profile_directory=profile_path)
 
-browser = webdriver.Firefox() #browser_profile=None
-#does using different browser_profiles reset cookies?
+driver = webdriver.Firefox(firefox_profile=ff_profile, firefox_binary=ff_binary) #browser_profile=None
 
 # things that could be useful
-#function browser.add_cookie(cookie_dict) # do I need to save all cookies, then add them?
-#function browser.get_cookies() -- returns a set of dictionaries
-#function browser.delete_all_cookies()
 #function find_element(by='id') 
 
+driver.get('https://gmail.com')
+time.sleep(30)
+
+# need to find temp profile after browsing and copy contents over to the saved FF profile in 'ff_profiles/test_profile'
+
+# temp_profile_path = driver.firefox_profile.path #apparently this doesn't actually work?
+print('what does it tell me the FF temp profile is?')
+print(driver.firefox_profile.path)
+print('Go find the actual FF temp profile path')
+driver.get('about:support')
+box = driver.find_element_by_id('profile-dir-box')
+temp_profile_path = box.text
+print('FF temp profile path: ', temp_profile_path)
+
+print('now copy contents to save browsing history')
+cwd = os.getcwd()
+original_profile = cwd+'\\ff_profiles\\test_profile'
+print('saving profile ' + temp_profile_path + ' to ' + original_profile)
+os.system('xcopy ' + temp_profile_path + ' ' + original_profile + '/Y /G /K /R /E /S /C /H /Q')
+print('files should be copied')
+
+time.sleep(5)
+
+driver.quit() 
 
 #search for a product then click on something
-browser.get('https://youtube.com')
-yt_search = browser.find_elements_by_name('search_query')[0]
-yt_search.send_keys('tennis')
-yt_search.submit()
-time.sleep(3)
-videos = browser.find_elements_by_id('video-title')
-videos[random.randrange(5)].click()
-time.sleep(3)
-c1 = browser.get_cookies()
-print("First YT visit cookies:")
-print(c1)
-print('\n')
-#go to a specific channel and click on a random video or two
-browser.get('https://www.google.com/search?q=pizza')
-browser.get('https://amazon.com')
-# set up an email address for them and visit it or send an email
-time.sleep(2)
-browser.get('https://youtube.com')
-yt_search = browser.find_elements_by_name('search_query')[0]
-yt_search.send_keys('pizza')
-yt_search.submit()
-time.sleep(3)
-videos = browser.find_elements_by_id('video-title')
-videos[random.randrange(5)].click()
-time.sleep(3)
-c2 = browser.get_cookies()
-print("second YT visit cookies:")
-print(c2)
+#driver.get('https://youtube.com')
+#yt_search = browser.find_elements_by_name('search_query')[0]
+#yt_search.send_keys('tennis')
+#yt_search.submit()
+#time.sleep(3)
+#videos = browser.find_elements_by_id('video-title')
+#videos[random.randrange(5)].click()
+#print("second YT visit cookies:")
+#print(c2)
 
 # all profiles visit zillow or show interest in purchasing a home, getting a loan
 
@@ -70,20 +77,6 @@ print(c2)
 # I could save the list of cookie dictionaries, then load them each time I start up
 # do I still need different versions / installations of Firefox to protect against stateless tracking?
 # won't my ip adress still be the same for each fake profile?
-#browser.quit() 
-browser.close()
 
-
-# Ideas for different profiles:
-# - sports profile
-# - video game profile
-# - artist profile
-# - musician profile
-# - online shopping profile
-# - news articles (left-leaning) - interests in Race - Black Panther, Judas & Black Messiah, Kendrick Lamar, Ibram X Kendi, etc
-# - fox news and alt-right
-# - one person only looks up info about housing / mortgage loans (control profile 1)
-# - one profile doesn't have any browsing history (control profile 2)
-# - do stereotype reinforcements cause an ethical issue?
 
 
